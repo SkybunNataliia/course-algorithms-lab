10c49f7a9729fdd9eec12fc930abc77f18c6b817
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-11-11 12:44:35 +0100

    Add insertion_sort function

--- asd-labs/sorting/sorting.py
+++ asd-labs/sorting/sorting.py
@@ -4,18 +4,26 @@ sizes = [10,100,1000,2000,5000]
 sys.setrecursionlimit(2 * max(sizes))
 
 def selection_sort(a):
-    n = len(a) - 1
-    for i in range(n):
-        # Interval to consider
+    for i in range(len(a) - 1):
         interval = a[i:]
         # Min index of entire array, with offset
         min_index = i + interval.index(min(interval))
         # Swap of elements
         a[i], a[min_index] = a[min_index], a[i]
-    return a
 
 def insertion_sort(a):
-    pass
+    n = len(a)
+    # Start from second element
+    for i in range(1, n):
+        # Elem to insert
+        pivot = a[i]
+        j = i - 1
+        # Right shirt of elements
+        while j >= 0 and a[j] > pivot:
+            a[j + 1] = a[j]
+            j -= 1
+        # Insert of pivot
+        a[j + 1] = pivot
 
 def bubble_sort(a):
     pass
@@ -30,6 +38,7 @@ if __name__ == '__main__':
     import test_utils
     tests = [
         (([7, 3, 5, 2, 1, 4, 6],), [1, 2, 3, 4, 5, 6, 7], 'random list'),
+        (([1,2,3],), [1,2,3], 'ordered list'),
     ]
     sorting_algorithms = [selection_sort, insertion_sort, bubble_sort, merge_sort, quick_sort]
     test_utils.test_all(tests, sorting_algorithms)
